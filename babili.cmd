7d2332aa89749d4c19f13fba7bdedb682ca9e7f0
wilk <szymon.wilk@cs.put.poznan.pl>
2017-08-17 11:26:59 -0400

    Revert "Moved to Ionic and revised/expanded existing mock-ups"
    This reverts commit 79f3394cb7739dcd8fe36874bfbc09522448f35b.

--- babili.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\babili\bin\babili.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\babili\bin\babili.js" %*
-)

419ddb508bbc9dda85c0565f9152a4bbbc42872e
Rezaey7 <30473994+Rezaey7@users.noreply.github.com>
2017-08-25 15:18:39 -0400

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { ProsDetailPage } from './../pages/pros-detail/pros-detail';
+import { IonicStorageModule } from '@ionic/storage';
+import { DocumentViewer } from '@ionic-native/document-viewer';
 import { ProsPage } from './../pages/pros/pros';
 import { VideoPage } from './../pages/video/video';
+import { ManualPage } from './../pages/manual/manual';
+import { SlidesPage } from './../pages/slides/slides';
 import { InterviewDetailPage } from './../pages/interview-detail/interview-detail';
+import { BarriersToEngagementPage } from './../pages/barriers-to-engagement/barriers-to-engagement';
+
+
 import { InterviewPage } from './../pages/interview/interview';
 import { ReportingPage } from './../pages/reporting/reporting';
 import { BehaviorChangePage } from './../pages/behavior-change/behavior-change';
@@ -12,6 +19,14 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
+import { ReportingAdherencePage } from './../pages/reporting-adherence/reporting-adherence';
+import { RiskyEventsPage } from './../pages/risky-events/risky-events';
+import { SymptomsPage } from './../pages/symptoms/symptoms';
+import { WeekReportPage } from './../pages/week-report/week-report';
+import { DailySummaryPage } from './../pages/daily-summary/daily-summary';
+
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -28,11 +43,20 @@ import { HomePage } from '../pages/home/home';
     InterviewDetailPage,
     VideoPage,
     ProsPage,
-    ProsDetailPage
+    ProsDetailPage,
+    ManualPage,
+    SlidesPage,
+    ReportingAdherencePage,
+    RiskyEventsPage,
+    SymptomsPage,
+    WeekReportPage,
+    DailySummaryPage,
+      BarriersToEngagementPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+      IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -46,7 +70,15 @@ import { HomePage } from '../pages/home/home';
     InterviewDetailPage,
     VideoPage,
     ProsPage,
-    ProsDetailPage
+    ProsDetailPage,
+    ManualPage,
+    SlidesPage,
+    ReportingAdherencePage,
+    RiskyEventsPage,
+    SymptomsPage,
+    WeekReportPage,
+    DailySummaryPage,
+      BarriersToEngagementPage
   ],
   providers: [
     StatusBar,

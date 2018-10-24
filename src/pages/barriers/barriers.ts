15580e3e220f4380529459ab834b3d93347b029b
sysmon37 <sysmon37>
2018-10-24 19:15:50 +0200

    Minor tweaks

--- src/pages/barriers/barriers.ts
+++ src/pages/barriers/barriers.ts
@@ -23,11 +23,11 @@ export class BarriersPage {
     this.items = [
       {title: "I do not fully understand what my doctor, nurse or the people at my pharmacy have explained to me so far", action: "", targetPage: ""},
       {title: "I can not mention the names of my medicines and their scope without hesitation", action: "", targetPage: ""},
-      {title: "I believe medicines are all poisonous and I should avoid taking medicines at all if possible", action: "", targetPage: ""},
+      {title: "I believe all medicines have adverse side effects and I should avoid taking medicines at all if possible", action: "", targetPage: ""},
       {title: "I feel basically healthy, therefore I am sometimes unsure whether I really have to take my medicines daily", action: "", targetPage: ""},
-      {title: "I fail to take my medicines every day automatically at a fixed time or on fixed occasions", action: "Use basic techniques to create reminders (post-it notes, calendar, alerts, etc.) and to establish routine", hint: "Use the calendar function to define relevant alerts", targetPage: "ActionPlanCalendarPage"},
+      {title: "I fail to take my medicines every day automatically at a fixed time or on fixed occasions", action: "Use basic techniques to create reminders (post-it notes, calendar, alerts, etc.) and to establish routine", hint: "Use action planning to define relevant alerts", targetPage: "ActionPlanCalendarPage"},
       {title: "Generally I often feel bad, and sometimes I feel discouraged and depressed", action: "", targetPage: ""},
-      {title: "I really would need help on an everyday basis (and particularly related to my treatment with medicines), but I do not get any help", action: "", targetPage: ""},
+      {title: "I really would need help on an everyday basis (and particularly related to my treatment with medicines), but I do not get any help", action: "Ask your provider about a social support group or a buddy who could provide tips", hint: "Read an interview with an engaged patient to learn about his experience", targetPage: ""},
 
       // {title: "Lack of positive reinforcement", disabled: 1, action: "Develop visuals reinforcing the positives of your behavior", targetPage: ""},
       // {title: "Inadequate support from family/friends", disabled: 1, action: "Meet to present expectations and scope of support", targetPage: ""},
